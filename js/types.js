eb66dfa777ecf5f5f3896daa8b827bc72de638c4
Sineway <sineway@yandex.com>
2023-02-22 18:55:05 +0300

    Добавит types.js

---
+++ js/types.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef ImageState
+ * @prop {number} id
+ * @prop {string} url
+ * @prop {string} description
+ * @prop {number} likes
+ * @prop {CommentState[]} comments
+ */
+
+/**
+ * @typedef CommentState
+ * @prop {number} id
+ * @prop {string} avatar
+ * @prop {string} message
+ * @prop {string} name
+ */
